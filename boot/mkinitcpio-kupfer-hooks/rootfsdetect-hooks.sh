7c524b6a86ec688d072f428870fabd5f9c22c600
Syboxez Blank <syboxez@gmail.com>
2022-05-31 00:09:20 -0500

    boot/mkinitcpio-kupfer-hooks: rewrite, use brute force partition scanning as fallback
    Co-authored-by: InsanePrawn <insane.prawny@gmail.com>

--- boot/mkinitcpio-kupfer-hooks/rootfsdetect-hooks.sh
+++ boot/mkinitcpio-kupfer-hooks/rootfsdetect-hooks.sh
@@ -1,21 +1,41 @@
 #!/usr/bin/ash
 
 run_hook() {
+  source /usr/lib/initcpio/hooks/hooks-functions.sh
+
   # Some bootloaders mess with those options (e.g. OnePlus 6), therefore we need to overwrite them
   # This is not nice, but there is no other way (at least right now)
   export rwopt=rw
   export init=/sbin/init
 
-  export root="/dev/disk/by-label/kupfer_root"
-  if [ ! -e "$root" ]; then
-    eval "$(cat /etc/kupfer/deviceinfo)"
-    deviceinfo_rootfs_image_sector_size="${deviceinfo_rootfs_image_sector_size:-512}"
-    losetup -P -b "$deviceinfo_rootfs_image_sector_size" /dev/loop0 "$deviceinfo_partitions_data"
-    partprobe /dev/loop0
-  fi
-  if [ ! -e "$(resolve_device "$root")" ]; then
-    # Still doesn't exist, so we don't have a root partition
-    echo "Failed to find root partition"
+  eval "$(cat /etc/kupfer/deviceinfo)"
+  deviceinfo_partitions_data="$deviceinfo_partitions_data"
+
+  for _root in "$root" "$deviceinfo_partitions_data" "/dev/disk/by-label/kupfer_root"; do
+    if [ -e "$_root" ] && scan_partitions "$_root" "LABEL=kupfer_root"; then
+      echo "exporting root='$RESULT'!"
+      export root="$RESULT"
+      return 0
+    fi
+  done
+
+  # Fall back to scanning for partitions
+
+  echo -n "kupfer_root not found as a raw partition"
+  [ -z "$deviceinfo_partitions_data" ] || echo -n " nor in data partition '$deviceinfo_partitions_data'"
+  echo -e ".\nScanning more partitions..."
+
+  unset RESULT
+
+  for part in /dev/sd*[0-9] ; do
+    ! scan_partitions "$part" "LABEL=kupfer_root" || break
+  done
+
+  if [ -n "$RESULT" ]; then
+    echo "Found kupfer_root at: $RESULT"
+    export root="$RESULT"
+  else
+    echo "Failed to find a partition labeled kupfer_root."
     unset root
   fi
 }
