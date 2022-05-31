4d66ac2b6354f036db63ff160814985e0b9e1c1a
Syboxez Blank <syboxez@gmail.com>
2022-05-31 00:09:20 -0500

    boot/mkinitcpio-kupfer-hooks: rewrite, use brute force partition scanning as fallback
    Co-authored-by: InsanePrawn <insane.prawny@gmail.com>

--- boot/mkinitcpio-kupfer-hooks/hooks-functions.sh
+++ boot/mkinitcpio-kupfer-hooks/hooks-functions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/ash
 
 panic() {
-	busybox echo "$@" >&2
+	echo "$@" >&2
 	exit 1
 }
 
@@ -10,14 +10,14 @@ panic() {
 check_partition_type()
 {
 	unset RESULT
-	local _partition="$1"
-	local _query="$2"
+	local partition="$1"
+	local query="$2"
 
-	busybox partprobe "$_partition" 2>/dev/null || true
+	busybox partprobe "$partition" 2>/dev/null || true
 
-	if ( blkid -p "$_partition" | grep -q "$_query" ); then
-		busybox echo "check_partition_type found $_query at: $_partition"
-		export RESULT="$_partition"
+	if ( blkid "$partition" -t "$query" ); then
+		echo "check_partition_type found $query at: $partition"
+		export RESULT="$partition"
 		return 0
 	fi
 	return 1
@@ -27,20 +27,21 @@ check_partition_type()
 # $1 = Partition to scan
 setup_loopdev()
 {
-	local _partition="$1"
+	local partition="$1"
 
 	# Get sector size from deviceinfo
 	eval "$(cat /etc/kupfer/deviceinfo)"
 	deviceinfo_rootfs_image_sector_size="${deviceinfo_rootfs_image_sector_size:-512}"
 
 	local _loopdev="$(losetup -f)"
-	losetup -P -b "$deviceinfo_rootfs_image_sector_size" "$_loopdev" "$_partition"
+	losetup -P -b "$deviceinfo_rootfs_image_sector_size" "$_loopdev" "$partition"
 	busybox partprobe "$_loopdev"
 
-	if [ -e ${_loopdev}p?* ]; then
+	if [ -n "$(ls "${_loopdev}"p?*)" ]; then
 		export LOOPDEV="$_loopdev"
 		return 0
 	else
+		echo "loopdev $_loopdev for $partition didn't show any subpartitions"
 		losetup -d "$_loopdev"
 		return 1
 	fi
@@ -49,14 +50,14 @@ setup_loopdev()
 # Probe partitions inside of a partition
 subpartition_probe()
 {
-	local _partition="$1"
-	local _query="$2"
+	local partition="$1"
+	local query="$2"
 
-	setup_loopdev "$_partition" || return 1
+	setup_loopdev "$partition" || return 1
 
 	# Scan all subpartitions
 	for subpart in "$LOOPDEV"p?*; do
-		! check_partition_type "$subpart" "$_query" || return 0
+		! check_partition_type "$subpart" "$query" || return 0
 	done
 
 	return 1
@@ -70,20 +71,18 @@ subpartition_probe()
 # $2 must be in the format 'TYPE=query', for example 'LABEL=kupfer_boot'
 scan_partitions()
 {
-	local _partition="$1"
-	local _query="$2"
+	local partition="$1"
+	local query="$2"
 
-	busybox echo "_partition is $_partition"
-	busybox echo "_query is $_query"
+	echo "scanning $partition for $query"
 	# Probe partition directly first
-	! check_partition_type "$_partition" "$_query" || return 0
+	! check_partition_type "$partition" "$query" || return 0
 
 	# Then scan for subpartitions
-	part_table_type=$(blkid -p -s PTTYPE -o value -n dos,gpt $_partition)
+	part_table_type="$(blkid -p -s PTTYPE -o value -n dos,gpt "$partition")"
 	if [ "$part_table_type" = "gpt" ] || [ "$part_table_type" = "dos" ]; then
-		busybox echo "Found subpartitions at: $_partition"
-		setup_loopdev "$_partition" || return 1
-		! subpartition_probe "$_partition" "$_query" || return 0
+		echo "Found partition table ($part_table_type) at: $partition"
+		! subpartition_probe "$partition" "$query" || return 0
 	fi
 
 	return 1
