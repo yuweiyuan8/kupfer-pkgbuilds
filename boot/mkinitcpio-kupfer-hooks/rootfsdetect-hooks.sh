5b826139405590d9b997d4b4c2970d2b66d2c21f
Syboxez Blank <@Syboxez:matrix.org>
2022-12-10 21:30:33 -0600

    boot/{initsquared, mkinitcpio-kupfer-hooks}: Prioritize boot-deploy outputs

--- boot/mkinitcpio-kupfer-hooks/rootfsdetect-hooks.sh
+++ boot/mkinitcpio-kupfer-hooks/rootfsdetect-hooks.sh
@@ -11,8 +11,14 @@ run_hook() {
   eval "$(cat /etc/kupfer/deviceinfo)"
   deviceinfo_partitions_data="$deviceinfo_partitions_data"
 
+  if [ -n "$kupfer_root_uuid" ]; then
+    query="UUID=${kupfer_root_uuid}"
+  else
+    query="LABEL=kupfer_root"
+  fi
+
   for _root in "$root" "$deviceinfo_partitions_data" "/dev/disk/by-label/kupfer_root"; do
-    if [ -e "$_root" ] && scan_partitions "$_root" "LABEL=kupfer_root"; then
+    if [ -e "$_root" ] && scan_partitions "$_root" "$query"; then
       echo "exporting root='$RESULT'!"
       export root="$RESULT"
       return 0
@@ -21,21 +27,21 @@ run_hook() {
 
   # Fall back to scanning for partitions
 
-  echo -n "kupfer_root not found as a raw partition"
+  echo -n "${query} not found as a raw partition"
   [ -z "$deviceinfo_partitions_data" ] || echo -n " nor in data partition '$deviceinfo_partitions_data'"
   echo -e ".\nScanning more partitions..."
 
   unset RESULT
 
   for part in /dev/sd*[0-9] ; do
-    ! scan_partitions "$part" "LABEL=kupfer_root" || break
+    ! scan_partitions "$part" "$query" || break
   done
 
   if [ -n "$RESULT" ]; then
-    echo "Found kupfer_root at: $RESULT"
+    echo "Found rootfs ${query} at: $RESULT"
     export root="$RESULT"
   else
-    echo "Failed to find a partition labeled kupfer_root."
+    echo "Failed to find a partition ${query}."
     unset root
   fi
 }
