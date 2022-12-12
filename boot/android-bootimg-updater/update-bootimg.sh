e693c76e90f4f18119807c2076b49840022046a9
Syboxez Blank <@Syboxez:matrix.org>
2022-12-11 20:57:24 -0600

    boot/android-bootimg-updater: Use boot-deploy to generate boot.img

--- boot/android-bootimg-updater/update-bootimg.sh
+++ boot/android-bootimg-updater/update-bootimg.sh
@@ -7,54 +7,36 @@ if [ "$(id -u)" -ne "0" ]; then
   exit 1
 fi
 
-if [ ! -f /etc/kupfer/deviceinfo ]; then
-  echo "No deviceinfo found at /etc/kupfer/deviceinfo. This might be intentional"
-  exit 1
-fi
-
-eval "$(cat /etc/kupfer/deviceinfo)"
-
 unset initramfs_file
 if [ -f "/boot/initramfs-initsquared.img" ]; then
-  initramfs_file="/boot/initramfs-initsquared.img"
+  initramfs_file="initramfs-initsquared.img"
 elif [ -f "/boot/initramfs-linux.img" ]; then
-  initramfs_file="/boot/initramfs-linux.img"
+  initramfs_file="initramfs-linux.img"
 elif [ -z "$1" ]; then
-  echo "/boot/initramfs-initsquared.img or /boot/initramfs-linux.img not found. initramfs location must be specified via argument."
+  echo "/boot/initramfs-initsquared.img or /boot/initramfs-linux.img not found. initramfs file name (located in /boot) must be specified via argument."
+  exit 1
 else
   initramfs_file="$1"
 fi
 
 dir=$(mktemp -d)
 
-cat /boot/Image.gz "/boot/dtbs/$deviceinfo_dtb.dtb" >"$dir/Image.gz-dtb"
+echo "Generating new boot.img with initramfs $initramfs_file"
 
-echo "Generating new aboot.img with initramfs $initramfs_file"
-
-mkbootimg \
-  --kernel "$dir/Image.gz-dtb" \
-  --ramdisk "$initramfs_file" \
-  --base "$deviceinfo_flash_offset_base" \
-  --second_offset "$deviceinfo_flash_offset_second" \
-  --kernel_offset "$deviceinfo_flash_offset_kernel" \
-  --ramdisk_offset "$deviceinfo_flash_offset_ramdisk" \
-  --tags_offset "$deviceinfo_flash_offset_tags" \
-  --pagesize "$deviceinfo_flash_pagesize" \
-  --cmdline "kupfer earlymodules=${deviceinfo_modules_initfs// /,} console=tty0" \
-  -o /boot/aboot.img
+boot-deploy -i "$initramfs_file" -k Image.gz -d /boot -o /boot -c /etc/kupfer/deviceinfo
 
 qhypstub_offset="508KiB"
 qhypstub_size="4KiB"
 lk2nd_bootimg_offset="512KiB"
 
-flash_file="/boot/aboot.img"
+flash_file="/boot/boot.img"
 if [[ "$deviceinfo_lk2nd" == "true" ]]; then
-  echo "Generating new aboot.bin for lk2nd device"
-  flash_file="/boot/aboot.bin"
+  echo "Generating new boot.bin for lk2nd device"
+  flash_file="/boot/boot.bin"
   dd if=/dev/zero of="$flash_file" bs="$lk2nd_bootimg_offset" count=1
   dd of="$flash_file" bs="$qhypstub_offset" count=1 conv=notrunc if=/boot/lk2nd.img
   dd of="$flash_file" bs="$qhypstub_size" count=1 conv=notrunc seek="$qhypstub_offset" oflag=seek_bytes if=/boot/qhypstub.bin
-  dd of="$flash_file" conv=notrunc oflag=append if=/boot/aboot.img
+  dd of="$flash_file" conv=notrunc oflag=append if=/boot/boot.img
 fi
 
 AB_SLOT_SUFFIX=$(grep -o 'androidboot\.slot_suffix=..' /proc/cmdline | cut -d "=" -f2)
@@ -65,36 +47,36 @@ if [ -e "$ABOOT_PART" ]; then
 
   ABOOT_PART="$(readlink -f "$ABOOT_PART")"
 
-  dd if="$ABOOT_PART" of=/tmp/aboot.bin
+  dd if="$ABOOT_PART" of=/tmp/boot.bin
   if [[ "$deviceinfo_lk2nd" == "true" ]]; then
-    dd if=/tmp/aboot.bin of=/tmp/aboot.img skip="$lk2nd_bootimg_offset" iflag=skip_bytes
+    dd if=/tmp/boot.bin of=/tmp/boot.img skip="$lk2nd_bootimg_offset" iflag=skip_bytes
   else
-    cp /tmp/aboot.bin /tmp/aboot.img
+    cp /tmp/boot.bin /tmp/boot.img
   fi
 
   permanent=false
-  if [[ "$(file /tmp/aboot.img)" != "/tmp/aboot.img: data" ]]; then
-    cmdline=$(unpack_bootimg --boot_img /tmp/aboot.img --out "$dir" | grep -E "^command line args")
+  if [[ "$(file /tmp/boot.img)" != "/tmp/boot.img: data" ]]; then
+    cmdline=$(unpack_bootimg --boot_img /tmp/boot.img --out "$dir" | grep -E "^command line args")
     if [[ "$cmdline" == *"kupfer"* ]]; then
       permanent=true
     fi
   fi
 
   if [[ "$permanent" == "true" ]]; then
-    echo "Running with permanent aboot.img"
-    echo "Flashing new aboot.img"
+    echo "Running with permanent boot.img"
+    echo "Flashing new boot.img"
     dd if="$flash_file" of="$ABOOT_PART"
   elif [[ "$1" == "-f" ]]; then
-    echo "Flashing new aboot.img"
+    echo "Flashing new boot.img"
     dd if="$flash_file" of="$ABOOT_PART"
   else
-    echo "Running with temporary aboot.img"
-    echo "Skip flashing new aboot.img"
+    echo "Running with temporary boot.img"
+    echo "Skip flashing new boot.img"
   fi
 else
   echo "Not running on the device"
-  echo "Skip flashing new aboot.img"
+  echo "Skip flashing new boot.img"
 fi
 
-rm -rf "$dir" /tmp/aboot.bin /tmp/aboot.img
+rm -rf "$dir" /tmp/boot.bin /tmp/boot.img
 
